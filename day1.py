bd22ef550b12e345d7665948f5dfa92db835f876
NicolaWidmer <widmenic@student.ethz.ch>
2021-12-01 11:56:42 +0100

    solved day one

--- day1.py
+++ day1.py
@@ -1,9 +1,3 @@
 f=open("/Users/Nicola/git/AdventOfCode2021/files/in1.txt","r")
-ans=0
-m=[]
-for line in f:
-    m.append(int(line))
-for i in range(len(m)-3):
-    if(m[i]<m[i+3]):
-        ans+=1
-print(ans)
+m=list(map(int,f))
+print(len([i for i,j in zip(m,m[3:]) if i<j]))
