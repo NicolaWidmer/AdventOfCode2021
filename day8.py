9a75b0d1e4a8d4e38e92b8592d62b0fd5f6a5d0a
NicolaWidmer <widmenic@student.ethz.ch>
2021-12-08 11:36:38 +0100

    day 8 ugly but fast solution

---
+++ day8.py
@@ -0,0 +1,47 @@
+def cont(s1,s2):
+    for c2 in s2:
+        if not (c2 in s1):
+            return False
+    return True
+
+f=open("files/day8.txt","r")
+
+nums = []
+for line in f:
+    nums.append((line.split('|')[0].split(),line.split('|')[1].split()))
+ans=0
+for enc,res in nums:
+    dicts=[""]*10
+    for e in enc:
+        if len(e)==2:
+            dicts[1]=e
+        if len(e)==3:
+            dicts[7]=e
+        if len(e)==4:
+            dicts[4]=e
+        if len(e)==7:
+            dicts[8]=e
+    dicts[9]=[e for e in enc if (len(e)==6 and cont(e,dicts[4]))][0]
+    for e in [e for e in enc if len(e)==6 and e!=dicts[9]]:
+        if(cont(e,dicts[1])):
+           dicts[0]=e
+        else:
+           dicts[6]=e
+    for e in [e for e in enc if len(e)==5]:
+        if(cont(e,dicts[1])):
+            dicts[3]=e
+        elif (cont(dicts[9],e)):
+            dicts[5]=e
+        else:
+           dicts[2]=e
+    curans=0
+    for r in res:
+        curans*=10
+        for i in range(10):
+            if cont(r,dicts[i]) and cont(dicts[i],r):
+                curans+=i
+
+    ans+=curans
+
+
+print(ans)
